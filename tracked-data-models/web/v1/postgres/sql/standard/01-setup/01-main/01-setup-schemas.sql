7e52c80a53c403dfa512fbec22b4be16e210efb5
alexopenline <alex@openline.ai>
2023-01-26 11:11:28 +0200

    Auto create schema and table for tracker configurations

--- tracked-data-models/web/v1/postgres/sql/standard/01-setup/01-main/01-setup-schemas.sql
+++ tracked-data-models/web/v1/postgres/sql/standard/01-setup/01-main/01-setup-schemas.sql
@@ -1,2 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS {{.output_schema}};
 CREATE SCHEMA IF NOT EXISTS {{.scratch_schema}};
+CREATE SCHEMA IF NOT EXISTS {{.tenant_schema}};
--- tracked-data-models/web/v1/postgres/sql/standard/01-setup/01-main/02-setup-metadata.sql
+++ tracked-data-models/web/v1/postgres/sql/standard/01-setup/01-main/02-setup-metadata.sql
@@ -1,5 +1,13 @@
 CREATE SEQUENCE IF NOT EXISTS {{.output_schema}}.shared_id_seq START 1;
 
+-- Table for trackers configurations
+CREATE TABLE IF NOT EXISTS {{.tenant_schema}}.{{.tracker_table}} (
+    id serial primary key,
+    tenant varchar(64) not null,
+    tracker_name varchar(255) not null unique,
+    app_id varchar(128) not null
+);
+
 -- A table storing an identifier for this run of a model - used to identify runs of the model across multiple modules/steps (eg. base, page views share this id per run)
 CREATE TABLE IF NOT EXISTS {{.scratch_schema}}.metadata_run_id{{.entropy}} (
     run_id TIMESTAMP
